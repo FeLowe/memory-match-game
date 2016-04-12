56e123b70704edce385028e0d8a64b0129f848f8
Fernanda Lowe <fe_lowe@hotmail.com>
2016-04-12 11:14:13 -0700

    shuffle the cards so that they appear randomly on the game board

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //BUSINESS LOGIC//
-function Game (cards, cardsTwo){ //<--object--//
-  this.cardsOne = cards;
-  this.cardsTwo = cardsTwo;
+function Game (cards1, cards2){ //<--object--//
+  this.cardsOne = cards1;
+  this.cardsTwo = cards2;
 }
 
 var buildCards = function(){ //---generates an array from 1 to 6 and pushes each # to empty cardsArray--//
@@ -24,41 +24,64 @@ var buildTotalArray = function(cardsOneArray, cardsTwoArray){ ///- 2 loops that 
   return totalArray;
 }
 
+function Card (cardValue, className){
+  this.cardValue = cardValue;
+  this.class = className;
+}
+
 
 //USER LOGIC//
 $(document).ready(function(){
-  var cardsOne = buildCards();
-  var cardsTwo = buildCards();
-  var newGame = new Game(cardsOne, cardsTwo);
 
+  var cards1 = buildCards();
+  var cards2 = buildCards();
+  var newGame = new Game(cards1, cards2);
   var combinedArrays = buildTotalArray(newGame.cardsOne, newGame.cardsTwo);
+    var icons = ["sun", "water", "fire", "moon", "wind", "earth"];
+
+  var shuffledArray = [];
+  var cardObjectsArray = [];
+
+
+  counter2 = 0
+  for(i=0; i < combinedArrays.length; i++){
+    var newCard = new Card(combinedArrays[i], icons[counter2]);
+    cardObjectsArray.push(newCard);
+     counter2 = counter2 + 1
+     if (counter2 === 6){
+       counter2 = 0;
+     }
+  }
+
+
+  var lengthOfCardObjectsArray = cardObjectsArray.length;
+
+
+  for(i = 0; i < (shuffledArray.length + cardObjectsArray.length); i++){
+    var randomValue = cardObjectsArray.splice((Math.floor(Math.random() * lengthOfCardObjectsArray)), 1);
+    shuffledArray.push(randomValue);
+    lengthOfCardObjectsArray = lengthOfCardObjectsArray - 1;
+
+  }
+
+  var newArray = [];
+
+  for(i = 0; i < (shuffledArray.length + cardObjectsArray.length); i++){
+    newArray = newArray.concat(shuffledArray[i]);
+  }
+
   var hTMLelement = $(".col-xs-4");
-  var icons = ["sun", "water", "fire", "moon", "wind", "earth"];
 
 
 
-    var counter = 0;
-    for(i = 0; i < 12; i++){
-      $(hTMLelement[i]).append('<li class="' + icons[counter] + '">' + combinedArrays[i] + '</li>');
-      counter += 1;
-      if (counter === 6) {
-        counter = 0;
-      }
 
-
+  var counter = 0;
+  for(i = 0; i < 12; i++){
+    $(hTMLelement[i]).append('<li class="' + newArray[i].class + '">' + newArray[i].cardValue + '</li>');
+    counter += 1;
+    if (counter === 6) {
+      counter = 0;
     }
-
-    // for(i = 6; i < 12; i++){
-    //   $(hTMLelement[i]).append('<li class="' + icons[i-6] + '">' + combinedArrays[i] + '</li>');
-    // }
-//
-// $(hTMLelement[i]).append("<li class='sun'>" + combinedArrays[i] + "</li>");
-// $(hTMLelement[i]).append("<li class='water'>" + combinedArrays[i] + "</li>");
-// $(hTMLelement[i]).append("<li class='fire'>" + combinedArrays[i] + "</li>");
-// $(hTMLelement[i]).append("<li class='moon'>" + combinedArrays[i] + "</li>");
-// $(hTMLelement[i]).append("<li class='wind'>" + combinedArrays[i] + "</li>");
-// $(hTMLelement[i]).append("<li class='earth'>" + combinedArrays[i] + "</li>");
-//
-
+  }
 
 });
