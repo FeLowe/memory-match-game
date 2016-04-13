6efa15c9fbbc6a155332b1815d254ddff9488b83
Fernanda Lowe and Kyle Wolfson <kyle@>
2016-04-13 11:53:18 -0700

    Fixed double-clicking on the same button problem, reset click counter display on game win, trimmed down UI logic by making a few functions to call instead.

--- js/scripts.js
+++ js/scripts.js
@@ -34,6 +34,15 @@ this.player = player;
 this.score = score;
 }
 
+function checkWin(winCounter) {
+  if (winCounter === 6) {
+    alert("Game Over!");
+    var highScore = new HighScore(player, turnCounter);
+    updateHighScores(highScore);
+    turnCounter = 0;
+  }
+}
+
 function updateHighScores(highScore) {
   for (var i = 0; i < 3; i++) {
     if (highScore.score < highScoreArray[i].score) {
@@ -41,6 +50,15 @@ function updateHighScores(highScore) {
       highScoreArray[i+1] = highScoreArray[i];
       highScoreArray[i] = highScore;
       highScoreArray = highScoreArray.slice(0, 3);
+      $(".highScores").show();
+      $(".user-name").empty();
+      $(".user-score").empty();
+        $(".user1-name").text(highScoreArray[0].player);
+        $(".user1-score").text(highScoreArray[0].score);
+        $(".user2-name").text(highScoreArray[1].player);
+        $(".user2-score").text(highScoreArray[1].score);
+        $(".user3-name").text(highScoreArray[2].player);
+        $(".user3-score").text(highScoreArray[2].score);
       break;
     }
   }
@@ -48,14 +66,15 @@ function updateHighScores(highScore) {
 
 defaultScores = new HighScore("Player", 100);
 var highScoreArray = [defaultScores, defaultScores, defaultScores];
+var turnCounter = 0;
+var player;
 
 
 //USER LOGIC//
 $(document).ready(function(){
-  var player;
   $("#player").submit(function(event){
     player = $("#playerName").val();
-    $("#player ul#name").append(player);
+    $("#player ul#name").text(player);
   });
   $("form").submit(function(event){
     event.preventDefault();
@@ -95,7 +114,7 @@ $(document).ready(function(){
       newArray = newArray.concat(shuffledArray[i]);
     }
 
-    var hTMLelement = $(".col-xs-4");
+    var hTMLelement = $(".main-content .col-xs-4");
 
     var counter = 0;
     for(i = 0; i < 12; i++){
@@ -105,46 +124,29 @@ $(document).ready(function(){
         counter = 0;
       }
     }
-      var winCounter = 0;
-      var lastClicked;
-      // var lastClicked = $(".hiddenPlaceholder");
-      var turnCounter = 0;
-      $("li").click(function(){
+    var winCounter = 0;
+    var lastClicked;
+    $("li").click(function(){
+      if (this != lastClicked){
         $(this).removeClass("back");
         $(this).show("li");
         turnCounter ++;
-        $("#score").text("Number of clicks: " + turnCounter);
         if ($(this).attr('class') === $(lastClicked).attr('class')) {
           lastClicked = $(".hiddenPlaceholder");
           winCounter ++;
-          if (winCounter === 6) {
-            alert("Game Over!");
-            var highScore = new HighScore(player, turnCounter);
-            updateHighScores(highScore);
-            console.log(highScoreArray);
-            // if (highScore.score < highScoreArray[0].score){
-            //   highScoreArray[2] = highScoreArray[1];
-            //   highScoreArray[1] = highScoreArray[0];
-            //   highScoreArray[0] = highScore;
-            // } else if (highScore.score < highScoreArray[1].score) {
-            //   highScoreArray[2] = highScoreArray[1];
-            //   highScoreArray[1] = highScore;
-            // } else if (highScore.score < highScoreArray[1].score) {
-            //   highScoreArray[2] = highScore;
-            // }
-            turnCounter = 0;
-
-
-          }
+          checkWin(winCounter);
         } else {
         $(lastClicked).addClass("back");
         lastClicked = this;
         }
-      });
-
+        $("#score").text("Number of clicks: " + turnCounter);
+      }
+    });
   });
 
 
   $("button").trigger("click");
+
+
 
 });
